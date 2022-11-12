28e308618c1316416afd2a7bb5c25ceb9af00ee7
Georges Sleimen <95876833+Georgiooooo@users.noreply.github.com>
2022-11-12 17:45:15 +0100

    READ ME

--- scraping script.py
+++ scraping script.py
@@ -291,4 +291,3 @@ for dates_des_tournois, tournament_name, court, surface, number in zip(dates_des
 
     table = table.append(table1, ignore_index=True)
     table.to_csv('out.csv', index=False)
-    
